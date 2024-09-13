ccc5d6bd37892c18ddbb9a6d196770b29b672ff7
liangpengyv <me@laoliang.ink>
2024-09-13 15:09:40 +0800

    build: 配置开发及构建环境

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-import { readFileSync } from 'node:fs'
-
-const pkg = JSON.parse(
-  readFileSync(new URL('../package.json', import.meta.url)),
-)
-
-console.log(`create-fluxpress version: ${pkg.version}`)
