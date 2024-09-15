092fe8ca0f8c53522edcf7d90a6d00b08e4773ec
liangpengyv <me@laoliang.ink>
2024-09-15 20:30:12 +0800

    feat: 更新系统和主题配置

--- template/theme-configs/official/fluxpress.config.classic.js
+++ template/theme-configs/official/fluxpress.config.classic.js
@@ -2,5 +2,12 @@
  * @type {import('@fluxpress/theme-classic').ThemeConfig}
  */
 export default {
-  per_page: 5,
+  need_data: ['issues'],
+  site: {
+    lang: 'zh',
+    title: 'FluxPress',
+    copyright: '2024',
+    author: 'LaoLiang',
+  },
+  per_page: 10,
 }
