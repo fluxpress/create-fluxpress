2ca5418b30cf234a0e0ef0b429e79ab87cb9145c
liangpengyv <me@laoliang.ink>
2024-09-15 21:08:57 +0800

    fix: APP_ROOT_PATH

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # create-fluxpress
+
+```shell
+npm create fluxpress@latest
+cd your-fluxpress-project
+npm install
+npx fluxpress fetch
+npx fluxpress generate
+npx fluxpress preview
+```
--- src/utils/package.test.ts
+++ src/utils/package.test.ts
@@ -3,6 +3,5 @@ import { getPackageLatestVersion } from './package.js'
 
 test('getPackageLatestVersion', async (t) => {
   const version = await getPackageLatestVersion('@fluxpress/core')
-  console.log(import.meta.url)
   t.is(typeof version, 'string')
 })
